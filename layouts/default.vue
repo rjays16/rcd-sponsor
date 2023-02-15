3b05e90a2c4c8374abf492018f67939a5cc630da
Allan Condiman <rjaycondiman81@gmail.com>
2023-02-16 06:36:31 +0800

    Moving Project Folder layouts

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <LayoutNavbar />
+    <LayoutSidebar />
+
+    <div class="content-wrapper">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      bodyAttrs: {
+        class: "hold-transition layout-fixed layout-navbar-fixed"
+      }
+    };
+  }
+};
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wrapper">
+		<div class="login-box">
+			<div class="card">
+				<article class="card-body text-center">
+                    <h1 class="text-uppercase w-100">
+						{{ error.statusCode === 404 ? "Page not found" : "An error occurred" }}
+					</h1>
+                    <a href="/">Return to Home page</a>
+				</article>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+		props: ["error"],
+        layout: 'login'
+    }
+</script>
+
+<style lang="scss" scoped>
+	.wrapper {
+		height: 100vh;
+	}
+	.login-box {
+		display: flex;
+		align-items: center;
+		flex-direction: column; 
+		justify-content: center;
+		width: 100%;
+		min-height: 100%;
+		padding: 20px;
+	}
+</style>
